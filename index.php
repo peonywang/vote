b2392b41d5166a5a861e0cf96bad1f511eac60d7
peonywang <fhjl1989@163.com>
2019-02-20 22:09:30 +0800

    'index'

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
+    <meta charset="UTF-8">
+    <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>首页</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>首页</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  首页
+  <div class="main main1">
+      <h1>
+          <img src="images/tit.png" alt="评选">
+      </h1>
+      <div class="exit">
+          <a href=""><img src="images/btn1.png" alt="巾帼标兵"></a>
+          <a href=""><img src="images/btn2.png" alt="实践模范"></a>
+          <a href=""><img src="images/btn3.png" alt="安全服务"></a>
+      </div>
+      <div class="bottom">text text text</div>
+  </div>
 </body>
 </html>
